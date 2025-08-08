ac875bce4618147453451b4ff64038c85d83d800
ParsEmir123 <wfgmemir@gmail.com>
2025-08-08 18:17:59 +0300

    Added function getUserChoice to get what user picks.

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function getComputerChoice(){
     if(randomNum===2) return "Scissor";
     else return null;
 }
+
+
+function getUserChoice(){
+    choice = prompt("Rock, Paper or Scissor");
+    if(choice.toLowerCase() === "rock" || choice.toLowerCase() ===  "paper" ||choice.toLowerCase() === "scissor") return choice;
+    else return Error("Please type rock, paper or scissor");
+
+
+}
+
+
+
+    console.log(getComputerChoice());
+    console.log(getUserChoice());
